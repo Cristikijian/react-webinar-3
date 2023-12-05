b2ef691774643fdf2ef2f82f2346fd7384a62cbe
Cristikijian <cristikijian@gmail.com>
2023-12-05 10:30:53 +0300

    currency formatting refactoring

--- src/hooks/useMoneyFormatter.js
+++ src/hooks/useMoneyFormatter.js
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
 
 export function useMoneyFormatter () {
-  return useMemo(() => new Intl.NumberFormat("ru-RU", { style: "currency", currency: "RUB" }));
+  return useMemo(() => new Intl.NumberFormat("ru-RU", { style: "currency", currency: "RUB", minimumFractionDigits: 0, }));
 };
